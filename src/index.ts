d3d6718ffa84274f9c581eeccacdc1aa9d1868ab
Ahmet Buğra Yiğiter <54077855+yigiterdev@users.noreply.github.com>
2022-08-02 11:37:20 +0300

    feat: add util files

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello World!");
+if (typeof window !== "undefined") {
+  // Pollyfill for Buffer
+  (window as any).global = window;
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  window.Buffer = window.Buffer || require("buffer").Buffer;
+}
+
+// Components
+import PeraWalletConnect from "./PeraWalletConnect";
+
+export {PeraWalletConnect};
--- webpack.config.js
+++ webpack.config.js
@@ -18,14 +18,39 @@ const config = {
   module: {
     rules: [
       {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: '@svgr/webpack',
+            options: {
+              svgoConfig: {
+                plugins: [{ removeViewBox: false }],
+              },
+            },
+          },
+          'url-loader',
+        ],
+      },
+
+      {
         test: /\.(ts|tsx)$/i,
         loader: 'ts-loader',
         exclude: ['/node_modules/'],
       },
+
       {
         test: /\.s[ac]ss$/i,
         use: ['css-loader', 'postcss-loader', 'sass-loader'],
       },
+
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
