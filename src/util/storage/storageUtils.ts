d4d3e1692137b08eda05a11c5a6dedd313e9cda4
Ahmet Buğra Yiğiter <yigiterahmetbugra@gmail.com>
2022-09-16 20:11:30 +0300

    fix(local-storage): solve local storage problem

--- src/util/storage/storageUtils.ts
+++ src/util/storage/storageUtils.ts
@@ -16,9 +16,16 @@ function saveWalletDetailsToStorage(accounts: string[]) {
 }
 
 function resetWalletDetailsFromStorage() {
-  getLocalStorage()?.removeItem(PERA_WALLET_LOCAL_STORAGE_KEYS.WALLETCONNECT);
-  getLocalStorage()?.removeItem(PERA_WALLET_LOCAL_STORAGE_KEYS.WALLET);
-  getLocalStorage()?.removeItem(PERA_WALLET_LOCAL_STORAGE_KEYS.BRIDGE_URL);
+  return new Promise<undefined>((resolve, reject) => {
+    try {
+      getLocalStorage()?.removeItem(PERA_WALLET_LOCAL_STORAGE_KEYS.WALLETCONNECT);
+      getLocalStorage()?.removeItem(PERA_WALLET_LOCAL_STORAGE_KEYS.WALLET);
+      getLocalStorage()?.removeItem(PERA_WALLET_LOCAL_STORAGE_KEYS.BRIDGE_URL);
+      resolve(undefined);
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
 
 export {getLocalStorage, saveWalletDetailsToStorage, resetWalletDetailsFromStorage};
