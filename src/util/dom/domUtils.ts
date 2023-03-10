66a3960cfe4ef9ed3f6f699f34c7b2b444b0cbcf
Ahmet Buğra Yiğiter <yigiterahmetbugra@gmail.com>
2023-03-10 16:57:14 +0300

    fix: update tab opening max try count

--- src/util/dom/domUtils.ts
+++ src/util/dom/domUtils.ts
@@ -2,7 +2,7 @@ import appTellerManager, {PeraTeller} from "../network/teller/appTellerManager";
 import PeraWalletConnectError from "../PeraWalletConnectError";
 
 const WAIT_FOR_TAB_TRY_INTERVAL = 300;
-const WAIT_FOR_TAB_MAX_TRY_COUNT = 50;
+const WAIT_FOR_TAB_MAX_TRY_COUNT = 20;
 
 function getMetaInfo() {
   const metaTitle: HTMLElement | null = document.querySelector('meta[name="name"]');
