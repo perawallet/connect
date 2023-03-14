f63426843531cc6297ee07aa3e7bcc1311f3570b
Ahmet Buğra Yiğiter <yigiterahmetbugra@gmail.com>
2023-03-14 18:00:23 +0300

    feat: add ping mech for iframe

--- src/util/peraWalletConstants.ts
+++ src/util/peraWalletConstants.ts
@@ -4,10 +4,12 @@ const PERA_WALLET_APP_DEEP_LINK = isAndroid() ? "algorand://" : "algorand-wc://"
 const PERA_DOWNLOAD_URL = "https://perawallet.app/download/";
 
 function getPeraWebWalletURL(webWalletURL: string) {
+  console.log("webWalletURL", webWalletURL);
+
   return {
-    ROOT: `https://${webWalletURL}`,
-    CONNECT: `https://${webWalletURL}/connect`,
-    TRANSACTION_SIGN: `https://${webWalletURL}/transaction/sign`
+    ROOT: `https://dev.web.perawallet.app:3000/`,
+    CONNECT: `https://dev.web.perawallet.app:3000/connect`,
+    TRANSACTION_SIGN: `https://dev.web.perawallet.app:3000/transaction/sign`
   };
 }
 
