03624b8a1f3fc94c2c98b743d4ad3764e6c17373
Ahmet Buğra Yiğiter <yigiterahmetbugra@gmail.com>
2023-03-14 18:01:32 +0300

    fix: revert contant change

--- src/util/peraWalletConstants.ts
+++ src/util/peraWalletConstants.ts
@@ -4,12 +4,10 @@ const PERA_WALLET_APP_DEEP_LINK = isAndroid() ? "algorand://" : "algorand-wc://"
 const PERA_DOWNLOAD_URL = "https://perawallet.app/download/";
 
 function getPeraWebWalletURL(webWalletURL: string) {
-  console.log("webWalletURL", webWalletURL);
-
   return {
-    ROOT: `https://dev.web.perawallet.app:3000/`,
-    CONNECT: `https://dev.web.perawallet.app:3000/connect`,
-    TRANSACTION_SIGN: `https://dev.web.perawallet.app:3000/transaction/sign`
+    ROOT: `https://${webWalletURL}`,
+    CONNECT: `https://${webWalletURL}/connect`,
+    TRANSACTION_SIGN: `https://${webWalletURL}/transaction/sign`
   };
 }
 
