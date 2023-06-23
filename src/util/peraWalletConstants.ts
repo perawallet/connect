5224f94accf0bd277194163d46e3a382f96b509f
Yasin Çalışkan <yasincaliskandev@gmail.com>
2023-06-23 11:52:13 +0300

    chore(deep-link): Use `perawallet-wc://` deep link

--- src/util/peraWalletConstants.ts
+++ src/util/peraWalletConstants.ts
@@ -1,6 +1,4 @@
-import {isAndroid} from "./device/deviceUtils";
-
-const PERA_WALLET_APP_DEEP_LINK = isAndroid() ? "algorand://" : "algorand-wc://";
+const PERA_WALLET_APP_DEEP_LINK = "perawallet-wc://";
 const PERA_DOWNLOAD_URL = "https://perawallet.app/download/";
 
 function getPeraWebWalletURL(webWalletURL: string) {
