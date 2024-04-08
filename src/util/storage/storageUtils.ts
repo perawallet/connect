8331ba424da801a633a7e6334d3026cc567c6bf1
Yasin Çalışkan <yasincaliskandev@gmail.com>
2024-04-08 11:35:43 +0300

    Upgrade walletconnect packages

--- src/util/storage/storageUtils.ts
+++ src/util/storage/storageUtils.ts
@@ -1,7 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
 
 import {PeraWalletDetails, PeraWalletNetwork, PeraWalletPlatformType} from "../peraWalletTypes";
-
 import {PERA_WALLET_LOCAL_STORAGE_KEYS} from "./storageConstants";
 
 function getLocalStorage() {
