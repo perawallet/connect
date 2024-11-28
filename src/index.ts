382af33b2b55cf1474a7e8717baed6c525027d43
Yasin Çalışkan <yasincaliskandev@gmail.com>
2024-11-28 17:43:54 +0300

    Remove async import for Buffer

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
-(async () => {
-  if (typeof window !== "undefined") {
-    // Pollyfill for Buffer
-    (window as any).global = window;
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    window.Buffer = window.Buffer || (await import('buffer')).Buffer;
+import {Buffer} from 'buffer';
 
-    import("./App");
-  }
-})();
+if (typeof window !== "undefined") {
+  // Pollyfill for Buffer
+  (window as any).global = window;
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  window.Buffer = window.Buffer || Buffer;
+
+  import("./App");
+}
+
 
 import PeraWalletConnect from "./PeraWalletConnect";
 import {closePeraWalletSignTxnToast} from "./modal/peraWalletConnectModalUtils";
