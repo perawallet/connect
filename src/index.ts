a9b2be3bbc1cc2a380075024e78939adcb8010d8
Yasin Çalışkan <yasincaliskandev@gmail.com>
2024-11-28 12:34:54 +0300

    Upgrade `algosdk` to v3 & ESM bundle

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
-if (typeof window !== "undefined") {
-  // Pollyfill for Buffer
-  (window as any).global = window;
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  window.Buffer = window.Buffer || require("buffer").Buffer;
+(async () => {
+  if (typeof window !== "undefined") {
+    // Pollyfill for Buffer
+    (window as any).global = window;
+    // eslint-disable-next-line @typescript-eslint/no-var-requires
+    window.Buffer = window.Buffer || (await import('buffer')).Buffer;
 
-  import("./App");
-}
+    import("./App");
+  }
+})();
 
 import PeraWalletConnect from "./PeraWalletConnect";
 import {closePeraWalletSignTxnToast} from "./modal/peraWalletConnectModalUtils";
