ed20ee83df0f3945b32ef9396d4f829caa255cbd
Yasin Çalışkan <yasincaliskandev@gmail.com>
2026-01-12 16:34:56 +0300

    feat: add verifySignature function and parameter

--- src/util/array/arrayUtils.ts
+++ src/util/array/arrayUtils.ts
@@ -16,4 +16,23 @@ function shuffleArray<T>(items: T[]) {
   return newItems;
 }
 
-export {shuffleArray};
+/**
+ * ConcatArrays takes n number arrays and returns a joint Uint8Array
+ * @param arrs - An arbitrary number of n array-like number list arguments
+ * @returns [a,b]
+ */
+function concatArrays(...arrs: ArrayLike<number>[]) {
+  const size = arrs.reduce((sum, arr) => sum + arr.length, 0);
+  const c = new Uint8Array(size);
+  let offset = 0;
+
+  for (let i = 0; i < arrs.length; i++) {
+    c.set(arrs[i], offset);
+    offset += arrs[i].length;
+  }
+
+  return c;
+}
+
+
+export {shuffleArray, concatArrays};
